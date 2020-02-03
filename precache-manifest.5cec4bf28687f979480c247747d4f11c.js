c7b35f4e59df7a0a53be713caac7344572709d19
Xiaohan Zhang <xiaohan.zhang.xz@gmail.com>
2020-02-02 20:38:16 -0500

    Updates

---
+++ precache-manifest.5cec4bf28687f979480c247747d4f11c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786d1a1f30b0996c2757c45d514741a7",
+    "url": "/laser-tank/index.html"
+  },
+  {
+    "revision": "b0cd7d2ae78a8b8e94b8",
+    "url": "/laser-tank/static/css/main.5db76f4b.chunk.css"
+  },
+  {
+    "revision": "e10d6820399a70e070f3",
+    "url": "/laser-tank/static/js/2.b6a8a6f6.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/laser-tank/static/js/2.b6a8a6f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0cd7d2ae78a8b8e94b8",
+    "url": "/laser-tank/static/js/main.bba56ac4.chunk.js"
+  },
+  {
+    "revision": "84e704454b861e9dd9ee",
+    "url": "/laser-tank/static/js/runtime-main.d7c6593c.js"
+  }
+]);
